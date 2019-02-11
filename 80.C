d08e0061b8538e10de809741f51717cc1c8c99a5
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-11 11:07:32 +0530

    Create 80.C

---
+++ 80.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,x=0,y,z,i=0,j;
+    int r[100000];
+    //clrscr();
+    printf("Input :");
+    scanf("%d%d",&a,&b);
+    printf("Output :\n");
+   
+      if(a>b)
+      y=a-b;
+      else
+      y=b-a;
+      printf("%d",y);
+    return 0;
+}
